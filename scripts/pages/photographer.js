c5629651f77217ba9d4d4242228e88789a13c916
htaylor91 <htaylor91@gmail.com>
2022-08-17 10:04:44 +0200

    feat(photographer.js): 🚧 add async function to retrieve individual photographer data via their unique id
    async function getPhotographers() is indentical to the function of the same name from index.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,39 @@
 //Mettre le code JavaScript lié à la page photographer.html
+//https://developer.mozilla.org/en-US/docs/Web/API/Location/search
+//https://developer.mozilla.org/en-US/docs/Web/API/URL/searchParams
+
+let params = (new URL(document.location)).searchParams;
+
+let id = parseInt(params.get('id')); // parse the photographer id from the URL
+
+//let name = params.get('name'); 
+
+async function getPhotographers() {
+    // Fetch data from the json
+    const response = await fetch('data/photographers.json');
+    const photographers = await response.json();
+    // return the array of photographers
+    return photographers;
+};
+
+console.log(getPhotographers()); //returns the two arrays, photographers & media
+
+async function getPhotographerById() {
+    getPhotographers()
+    .then(allResults => {
+        for (result of allResults.photographers) {
+            if (result.id === id) {
+                console.log(result.id);
+                console.log(result.name);
+                console.log(result.city);
+                console.log(result.country);
+                console.log(result.price);
+                console.log(result.tagline);
+                console.log(result.portrait);
+                console.log(result);
+            }
+        }
+    });
+}
+
+console.log(getPhotographerById());
