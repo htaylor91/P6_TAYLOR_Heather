0bf5fc2e9257065043ebe837aafac298bbb4beeb
htaylor91 <htaylor91@gmail.com>
2022-08-23 14:08:30 +0200

    docs(index.js): 💡 add comments to describe async functions in index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,32 @@
 async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
+    //Function to fetch data from the json
     const response = await fetch('data/photographers.json');
+     //Wait for the promise to resolve
     const photographers = await response.json();
-    // et bien retourner le tableau photographers seulement une fois
-    return photographers;
+    return photographers
+    //Return an object containing the 2 arrays from the .json - photographers[] and media[]
 };
 
-console.log(getPhotographers());
-
 async function displayData(photographers) {
+    ////Function to display all photographers
     const photographersSection = document.querySelector(".photographer_section");
-
     photographers.forEach((photographer) => {
+        //for each photographer in the .json array photographers[] ...
         const photographerModel = photographerFactory(photographer);
+        //photographerModel is all 6 photographers as objects
         const userCardDOM = photographerModel.getUserCardDOM();
+        //userCardDOM is the 6 HTML <article> elements
         photographersSection.appendChild(userCardDOM);
     });
 };
 
+
 async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
+    //Function to retrieve and display the photographer data for the index.html page
+    const {photographers} = await getPhotographers();
+    //{photographers} is an object containing the array photographers[] from the .json 
     displayData(photographers);
+    //Call the function to display all photographers' data
 };
 
 init();
-    
