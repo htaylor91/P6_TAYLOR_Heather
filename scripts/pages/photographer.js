ca5593f4a20b39e8e868c2d4905e7b6b7f2dd6c0
htaylor91 <htaylor91@gmail.com>
2022-08-24 10:05:28 +0200

    feat(pages/photographer.js): ✨ replace getPhotographerById() with displayData(photographer) and main(), update comments
    getPhotographerById() is replaced by the photographers.find() in main(). displayData(photographer) displays the specific photographer's data in the banner section when the main() function is called.

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,39 +1,42 @@
-//Mettre le code JavaScript lié à la page photographer.html
-//https://developer.mozilla.org/en-US/docs/Web/API/Location/search
+//The Javascript code linked to the photographer.html pages
+
+//"The searchParams readonly property of the URL interface returns a 
+//URLSearchParams object allowing access to the GET decoded query arguments contained in the URL."
 //https://developer.mozilla.org/en-US/docs/Web/API/URL/searchParams
 
 let params = (new URL(document.location)).searchParams;
-
-let id = parseInt(params.get('id')); // parse the photographer id from the URL
-
-//let name = params.get('name'); 
+let identity = parseInt(params.get('id')); //Parse the photographer's id from the URL
 
 async function getPhotographers() {
-    // Fetch data from the json
+    //Function to fetch data from the json
     const response = await fetch('data/photographers.json');
+     //Wait for the promise to resolve
     const photographers = await response.json();
-    // return the array of photographers
-    return photographers;
+    return photographers
+    //Return an object containing the 2 arrays from the .json - photographers[] and media[]
 };
 
-console.log(getPhotographers()); //returns the two arrays, photographers & media
+async function displayData(photographer) {
+    //Function to display the unique photographer's personal data in their page banner
+    const photographBanner = document.querySelector(".photographBanner");
+        const photographerModel = photographerFactory(photographer);
+        //photographerModel is the photographer data... 
+        //...and factory functions getUserCardDOM() and getPageDOM()
+        const pageDOM = photographerModel.getPageDOM();
+        console.log(pageDOM);
+        //pageDOM is the <article> in the banner of photographer.html
+        photographBanner.appendChild(pageDOM);
+};
 
-async function getPhotographerById() {
-    getPhotographers()
-    .then(allResults => {
-        for (result of allResults.photographers) {
-            if (result.id === id) {
-                console.log(result.id);
-                console.log(result.name);
-                console.log(result.city);
-                console.log(result.country);
-                console.log(result.price);
-                console.log(result.tagline);
-                console.log(result.portrait);
-                console.log(result);
-            }
-        }
-    });
-}
+async function main() {
+    //Main function to retrieve and display the data for the unique photographer.html page
+    const {photographers, media} = await getPhotographers();
+    //{photographers, media} is an object containing the arrays photographers[] and media[] from the .json 
+    let photographer = photographers.find(photographer => photographer.id === identity);
+    //find() the first photographer with an id matching the identity / the current URL
+    displayData(photographer);
+    //Call the function to display this specific photographer's personal data
 
-console.log(getPhotographerById());
+};
+
+main();
