7527cf82177663281bf8df838064b3167758aa54
htaylor91 <htaylor91@gmail.com>
2022-09-02 12:16:32 +0200

    feat(factories/media.js): ✨ add functions addLike() and addLikeToTotal(), add lightbox and likes event listeners, refactor
    refactor by replacing appendChild with append where there are multiple child elements, delete mediaContainer to make anchor the parent of images and videos

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -4,29 +4,29 @@ function mediaFactory(data) {
     const galleryImage = `assets/images/${photographerId}/${image}`;
     const galleryVideo = `assets/images/${photographerId}/${video}`;
     const galleryAlt = `${title}`;
+    const mediaIdentifier = `${id}`;
 
     const heartIcon = `assets/icons/heart.svg`;
 
     function getMediaDOM() {
         
         const article = document.createElement( 'article' );
+        article.setAttribute("class", 'articleMedia');
 
         const anchor = document.createElement( 'a' );
-        anchor.setAttribute("href", `./photographer.html?id=${photographerId}/${id}`);
+        //anchor.setAttribute("href", `./photographer.html?id=${photographerId}/${id}`);
+        anchor.setAttribute("class", 'articleMedia__anchor');
+        anchor.setAttribute("target", '_parent');
 
-        const mediaContainer = document.createElement( 'div' );
-        mediaContainer.setAttribute("class", 'mediaContainer');
-
-        article.appendChild(anchor);
-        anchor.appendChild(mediaContainer);
+        anchor.addEventListener("click", displayLightbox);
 
         function createImage() {
             const img = document.createElement( 'img' );
             img.setAttribute("src", galleryImage);
             img.setAttribute("alt", galleryAlt);
             img.setAttribute("class", 'mediaContainer__image');
-
-            mediaContainer.appendChild(img);
+            img.setAttribute("id", mediaIdentifier);
+            anchor.appendChild(img);
         };
 
         function createVideo() {
@@ -35,12 +35,14 @@ function mediaFactory(data) {
             video.setAttribute("class", 'mediaContainer__video');
             video.setAttribute("controls", 'controls');
             video.setAttribute("preload", "metadata");
+            video.setAttribute("aria-label", galleryAlt);
+            video.setAttribute("id", mediaIdentifier);
 
             const source = document.createElement('source');
-            source.setAttribute("src", galleryVideo + '#t=0.1');
+            source.setAttribute("src", galleryVideo + '#t=0.01');
             source.setAttribute("type", 'video/mp4');
 
-            mediaContainer.appendChild(video);
+            anchor.appendChild(video);
             video.appendChild(source);
         };
 
@@ -73,25 +75,55 @@ function mediaFactory(data) {
         mediaLikes.textContent = likes;
         mediaLikes.setAttribute("class", 'infoContainer__likesContainer__likes');
 
-        const mediaLikesButton = document.createElement('img');
-        mediaLikesButton.setAttribute("src", heartIcon);
-        mediaLikesButton.setAttribute("alt", 'likes');
-        mediaLikesButton.setAttribute("class", 'infoContainer__likesContainer__button');
+        const mediaLikesButton = document.createElement('button');
+        mediaLikesButton.setAttribute("class", 'infoContainer__likesContainer__button button button--like');
+        mediaLikesButton.setAttribute("type", 'button');
+
+        const mediaLikesButtonImage = document.createElement('img');
+        mediaLikesButtonImage.setAttribute("src", heartIcon);
+        mediaLikesButtonImage.setAttribute("alt", 'likes');
+        mediaLikesButtonImage.setAttribute("class", 'infoContainer__likesContainer__button__image');
+
+        article.append(anchor, infoContainer);
+
         
-        article.appendChild(infoContainer);
-        infoContainer.appendChild(mediaTextContainer);
-        infoContainer.appendChild(mediaLikesContainer);
+        infoContainer.append(mediaTextContainer, mediaLikesContainer);
 
-        mediaTextContainer.appendChild(mediaTitle);
-        mediaTextContainer.appendChild(mediaDate);
+        mediaTextContainer.append(mediaTitle, mediaDate);
 
-        mediaLikesContainer.appendChild(mediaLikes);
-        mediaLikesContainer.appendChild(mediaLikesButton);
-    
+        mediaLikesContainer.append(mediaLikes, mediaLikesButton);
+
+        mediaLikesButton.appendChild(mediaLikesButtonImage);
+
+        //Add up to 1 like per media asset with a click event
+       
+        function addLike() {
+            let likesCount = likes;
+            likesCount++;
+            mediaLikes.innerText = likesCount;
+            return likesCount;
+        };
+
+        mediaLikesButton.addEventListener("click", addLike);
+        
+        //Add new likes to likes total in the footer
+
+        function addLikeToTotal() {
+            const photographerFooterLikes = document.getElementById("totalLikes");
+            let totalLikesCount = photographerFooterLikes.innerText;
+            totalLikesCount++;
+            photographerFooterLikes.innerText = totalLikesCount;
+            mediaLikesButton.removeEventListener("click", addLikeToTotal);
+            return totalLikesCount;
+        }
+
+        mediaLikesButton.addEventListener("click", addLikeToTotal);
+
+        anchor.addEventListener("click", displayLightbox);
         return (article);
     }
 
     return { 
-       price, getMediaDOM
+       id, price, likes, getMediaDOM
     }
 }
