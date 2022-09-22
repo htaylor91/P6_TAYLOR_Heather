bd6fb150c197b6707b86c3b1f4f93b953755140c
htaylor91 <htaylor91@gmail.com>
2022-09-22 16:31:44 +0200

    feat(helpers/filter.js): 🔥 remove listbox constants and dropdown functions to reflect new native <select> dropdown
    native select dropdown code is in a separate helpers file

--- scripts/helpers/filter.js
+++ scripts/helpers/filter.js
@@ -1,52 +1,6 @@
 
 import {displayMedia, mainMedia} from "../pages/photographer.js";
 
-const listbox = document.getElementById("listbox");
-const filterArrow = document.getElementById("filterArrow");
-
-const firstLabel = document.getElementById("listbox-1");
-const secondLabel = document.getElementById("listbox-2");
-const thirdLabel = document.getElementById("listbox-3");
-
-const popularityBtn = document.getElementById("popularityBtn");
-const dateBtn = document.getElementById("dateBtn");
-const titleBtn = document.getElementById("titleBtn");
-
-const toggleArrow = document.getElementById("toggleArrow");
-
-function displayDropdown() {
-
-    listbox.setAttribute("aria-expanded", true);
-
-    secondLabel.classList.remove("hide");
-    thirdLabel.classList.remove("hide");
-
-    secondLabel.classList.add("show");
-    thirdLabel.classList.add("show");
-
-    toggleArrow.src = "assets/icons/arrow-up.svg";
-    filterArrow.removeEventListener("click", displayDropdown);
-    filterArrow.addEventListener("click", hideDropdown );
-    
-}
-
-function hideDropdown() {
-
-    listbox.setAttribute("aria-expanded", false);
-
-    secondLabel.classList.remove("show");
-    thirdLabel.classList.remove("show");
-    
-    secondLabel.classList.add("hide");
-    thirdLabel.classList.add("hide");
-
-    toggleArrow.src = "assets/icons/arrow-down.svg";
-    filterArrow.removeEventListener("click", hideDropdown);
-    filterArrow.addEventListener("click", displayDropdown);
-}
-
-filterArrow.addEventListener("click", displayDropdown);
-
 //Delete media assets from the gallery section
 const clearGallery = () => {
     const gallerySection = document.querySelector(".gallerySection");
@@ -59,71 +13,8 @@ const clearGallery = () => {
     removeAllChildNodes(gallerySection);
 };
 
-//Clear all filterSection list item children 
-//to make room to append new children to them
-//when a filter option is selected
-const clearLabel = () => {
-    function removeAllChildNodes(parent) {
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
-        }
-    }
-    removeAllChildNodes(firstLabel);
-    removeAllChildNodes(secondLabel);
-    removeAllChildNodes(thirdLabel);
-
-};
-
-function popularityDropdown() {
-    clearLabel();
-
-    firstLabel.removeAttribute("aria-selected", false);
-    firstLabel.appendChild(popularityBtn);
-    firstLabel.setAttribute("aria-selected", true);
-    
-    secondLabel.appendChild(dateBtn);
-    thirdLabel.appendChild(titleBtn);
-
-    hideDropdown();
-    listbox.focus();
-
-    sortByLikes();
-    
-}
-
-function dateDropdown() {
-    clearLabel();
-
-    firstLabel.removeAttribute("aria-selected", false);
-    firstLabel.appendChild(dateBtn);
-    firstLabel.setAttribute("aria-selected", true);
-    
-    secondLabel.appendChild(titleBtn);
-    thirdLabel.appendChild(popularityBtn);
-
-    hideDropdown();
-    listbox.focus();
-    sortByDate();
-}
-
-function titleDropdown() {
-    clearLabel();
-
-    firstLabel.removeAttribute("aria-selected", false);
-    firstLabel.appendChild(titleBtn);
-    firstLabel.setAttribute("aria-selected", true);
-
-    secondLabel.appendChild(popularityBtn);
-    thirdLabel.appendChild(dateBtn);
-
-    hideDropdown();
-    listbox.focus();
-    sortByTitle();
-    
-}
-
 //Sort by Likes
-async function sortByLikes() {
+export const sortByLikes = async () => {
     clearGallery();
     const mediaAssets = await mainMedia();
     const sortedLikes = mediaAssets.sort(function (a, b) {
@@ -131,10 +22,10 @@ async function sortByLikes() {
     });
     
     displayMedia(sortedLikes);
-}
+};
 
 //Sort by Title
-async function sortByTitle() {
+export const sortByTitle = async () => {
     clearGallery();
     const mediaAssets = await mainMedia();
     const sortedTitles = mediaAssets.sort(function(a, b) {
@@ -146,19 +37,16 @@ async function sortByTitle() {
     });
 
     displayMedia(sortedTitles);
-}
+};
 
 //Sort by Date
-async function sortByDate() {
+export const sortByDate = async () => {
     clearGallery();
     const mediaAssets = await mainMedia();
     const sortedDates= mediaAssets.sort(function(a, b) {
         return new Date(b.date) - new Date(a.date);
     });
 
+    console.table(sortedDates);
     displayMedia(sortedDates);
-}
-
-popularityBtn.addEventListener("click", popularityDropdown);
-dateBtn.addEventListener("click", dateDropdown);
-titleBtn.addEventListener("click", titleDropdown);
+};
